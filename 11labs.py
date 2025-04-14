61d62b06801559da3dc08b4fc9f769a6184919aa
pri-div <156355349+pri-div@users.noreply.github.com>
2025-04-14 23:22:00 +0530

    text to speech

--- 11labs.py
+++ 11labs.py
@@ -1,8 +1,20 @@
 from elevenlabs.client import ElevenLabs
-
+import elevenlabs
 client = ElevenLabs(
   api_key="sk_aa769546ed738d59fcbac85656c3c826920404ccbb432ece",
 )
 
-response = client.voices.get_all()
-print(response.voices)
+# response = client.voices.get_all()
+# print(response.voices)
+
+from elevenlabs import play
+
+
+audio = client.text_to_speech.convert(
+    text="helooooo",
+    voice_id="JBFqnCBsd6RMkjVDRZzb",
+    model_id="eleven_multilingual_v2",
+    output_format="mp3_44100_128",
+)
+
+elevenlabs.save(audio,"yashuu.mp3")
